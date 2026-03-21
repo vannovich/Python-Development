60b40bdc6a5a821dbae6ab475e97b29eace7776a
vannovich <awontuvannovich@gmail.com>
2026-03-21 10:53:29 +0100

    Day 1

---
+++ Day001/day1_exercise.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+
+city = input("Enter the City where you grow up: ")
+pet = input("Enter the name of your pet: ")
+print(f"Your band name could be {city.strip().upper()} {pet.strip().upper()}")
